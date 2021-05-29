aad623ed3118ece1b018580800fe89871ec42b75
Manish Nakar <manish.nakar@gmail.com>
2021-05-29 16:53:23 +0530

    setup done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const avatarSrc = 'https://instagram.fbom3-1.fna.fbcdn.net/v/t51.2885-19/s150x150/118723051_178503100553167_8751697531708993618_n.jpg?tp=1&_nc_ht=instagram.fbom3-1.fna.fbcdn.net&_nc_ohc=F0z3lD_zDhgAX_W_NLB&edm=AEF8tYYBAAAA&ccb=7-4&oh=175717351c54e1e01b93f9ba38d1b19a&oe=60B9FD85&_nc_sid=a9513d'
   const song = 'test Song'
   const likes = '50'
-  const url = 'http://localhost:3000/192120569_950642109045523_77023514642497228_n.mp4'
+  const url = './192120569_950642109045523_77023514642497228_n.mp4'
   const shares = '100'
 
 
